5105f079fd92c2df6ba6f734f820bfa2bdc10b91
e1essar <80064778+e1essar@users.noreply.github.com>
2024-05-17 14:51:01 +0700

    repl_user -> repluser

--- db/init.sql
+++ db/init.sql
@@ -4,20 +4,20 @@ BEGIN
         CREATE DATABASE db_bot;
     END IF;
 
-    IF NOT EXISTS (SELECT 1 FROM pg_user WHERE usename = 'repl_user') THEN
-        CREATE USER repl_user WITH REPLICATION ENCRYPTED PASSWORD 'replpass';
+    IF NOT EXISTS (SELECT 1 FROM pg_user WHERE usename = 'repluser') THEN
+        CREATE USER repluser WITH REPLICATION ENCRYPTED PASSWORD 'replpass';
     END IF;
 
     IF NOT EXISTS (SELECT 1 FROM pg_replication_slots WHERE slot_name = 'replication_slot') THEN
         PERFORM pg_create_physical_replication_slot('replication_slot');
     END IF;
 
-    GRANT CONNECT ON DATABASE db_bot TO repl_user;
-    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO repl_user;
-    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO repl_user;
-    GRANT pg_read_all_data TO repl_user;
-    GRANT pg_write_all_data TO repl_user;
-    GRANT ALL PRIVILEGES ON DATABASE db_bot TO repl_user;
+    GRANT CONNECT ON DATABASE db_bot TO repluser;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO repluser;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO repluser;
+    GRANT pg_read_all_data TO repluser;
+    GRANT pg_write_all_data TO repluser;
+    GRANT ALL PRIVILEGES ON DATABASE db_bot TO repluser;
     GRANT ALL PRIVILEGES ON DATABASE db_bot TO postgres;
 
     IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'email_addresses') THEN
